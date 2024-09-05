e0c5a2fa73c28c4735acfef47730aab3178266ba
taroBense <140390456+taroBense@users.noreply.github.com>
2024-09-05 12:27:18 +1200

    fix [v1.2.0]: make lineSensor a module, fix timing method

--- interfacing/mt280_qtrx-rc.py
+++ interfacing/mt280_qtrx-rc.py
@@ -1,65 +1,84 @@
 # Author: Taro Bense
-# ENGMT280 QTRX-HD-09RC interfacing module V0.2.1
+# ENGMT280 QTRX-HD-09RC interfacing module V1.2.0
+# Changelog: modularised code; improved timing method; finalised output method
 import RPi.GPIO as GPIO
-import time 
+import time
 
-# set pin naming convention
-GPIO.setmode(GPIO.BCM)
+class lineSensor:
+    def __init__(self, ctrl_pin, sensor_pins, max_value):
+        self.ctrl_pin = ctrl_pin # control pin
+        self.sensor_pins = sensor_pins # array of gpio pins connected to sensor
+        self.sensor_count = len(sensor_pins) # amount of sensors
+        # this is the maximum possible discharge time of the RC circuit
+        self.max_value = max_value
+        
+        # set pin naming convention
+        GPIO.setmode(GPIO.BCM)
 
-# define all data and control pins
-# v0.2.1 correct pin numbering
-CTRL = 18
-D_1 = 23
-D_2 = 20
-D_3 = 24
-D_4 = 16
-D_5 = 25
-D_6 = 12
-D_7 = 8
-D_8 = 1
-D_9 = 7
+        # set control pin (IR LED controller) to output
+        GPIO.setup(ctrl_pin, GPIO.OUT)
 
-data_array = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-timing_arr = []
-output_arr = []
+        # set data pin I/O lines to outputs
+        for pin in self.sensor_pins:
+            GPIO.setup(pin, GPIO.OUT)
 
-# turn on IR LEDs
-GPIO.setup(CTRL, GPIO.OUT)
-GPIO.output(CTRL, GPIO.HIGH)
+    def read(self):
+        sensor_values = [self.max_value] * self.sensor_count
 
-# make data pins output line
-GPIO.setup(data_array, GPIO.OUT)
+        # turn IR LEDs on
+        GPIO.output(CTRL_PIN, GPIO.HIGH)
 
-# set pins to high
-GPIO.output(data_array, GPIO.HIGH)
+        # drive data pins high
+        for i in range(self.sensor_count):
+            GPIO.output(self.sensor_pins[i], GPIO.HIGH)
 
-# wait 10 microsecs
-time.sleep(0.00001)
+        # wait for 10 microsecs
+        time.sleep(0.00001)  # 10 microseconds
 
-# set the data pins to inputs
-GPIO.setup(data_array, GPIO.IN)
+        # start timing
+        start_time = time.time()
 
-for d_pin in data_array:
-    # start the timing
-    timing_arr.append(time.time())
-    # read the values and send to output_arr
-    output_arr[d_pin] = GPIO.input(data_array[d_pin])
-     
-    while GPIO.input(data_array) == GPIO.HIGH:
-        pass
+        # set data I/O lines to input
+        for i in range(self.sensor_count):
+            GPIO.setup(self.sensor_pins[i], GPIO.IN)
+
+        # measure the discharge time
+        while True:
+            # convert to microseconds
+            elapsed_time = (time.time() - start_time) * 1000000
+
+            if elapsed_time > self.max_value:
+                break  # Stop if time exceeds the maximum threshold
+
+            for i in range(self.sensor_count):
+                if GPIO.input(self.sensor_pins[i]) == GPIO.LOW and elapsed_time < sensor_values[i]:
+                    sensor_values[i] = elapsed_time
+
+        # turn IR LEDs off
+        GPIO.output(CTRL_PIN, GPIO.LOW)
+
+        return sensor_values
     
-    end_time = time.time()
+    def cleanup(self):
+        GPIO.cleanup()
 
-# turn IR LEDs off
-GPIO.output(CTRL, GPIO.LOW)
+# this runs if python program is run instead of used as a module
+if __name__ == "__main__":
+    RESISTANCE = 220
+    CAPACITANCE = 2.2E-9
+    TIME_CONSTANT = RESISTANCE * CAPACITANCE
+    max_discharge = 6 * TIME_CONSTANT
 
-# calculate each dischrge time
-for duration in timing_arr:
-    timing_arr[duration] = end_time - timing_arr[duration]
-
-# output each duration for debugging
-for i in range(len(timing_arr) - 1):
-    print(f"Time for data to go low: {timing_arr[i]:.10f} seconds.")
-
-# cleanup gpio pins after use
-GPIO.cleanup()
+    # define control pin
+    CTRL_PIN = 18
+    # Example GPIO pins for the sensors
+    sensor_pins = [23, 20, 24, 16, 25, 12, 8, 1, 7]
+    
+    sensors = lineSensor(CTRL_PIN, sensor_pins, max_discharge)
+    
+    while True:
+        try:
+            sensor_values = sensors.read()
+            print(sensor_values)
+        finally:
+            sensors.cleanup()
