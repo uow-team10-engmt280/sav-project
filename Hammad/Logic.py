62dff4973bd409975c9cab8f244c63bbad82bf49
hammadmoh <152244486+hammadmoh@users.noreply.github.com>
2024-09-05 14:12:02 +1200

    Update Logic.py

--- Hammad/Logic.py
+++ Hammad/Logic.py
@@ -1,1 +1,5 @@
-# 
+# Read line Sensor info, moves or turns accordingly
+# Whilst moving if the distance sensor gpio goes high stop and turn servo and pickup object, then give the line sensor a timeout, allowing the sav to move even if its high for a time period.
+# Once line Sensor detects binary for parking, the sav stops moving
+# States are in while loop
+#
