8aabc48f4309db6e59d28734c6b6055a946502ba
hammadmoh <152244486+hammadmoh@users.noreply.github.com>
2024-09-06 12:01:36 +1200

    Update print stuff.py

--- Hammad/print stuff.py
+++ Hammad/print stuff.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 from machine_vision import panel_directions  # Import the 2-bit array from machine_vision.py
-from line_sensor import binary_array
+import line_sensor as ls
 
 class SAV:
     def __init__(self):
@@ -16,7 +16,26 @@ class SAV:
       
     def run(self):
         while True:
-          if 
+            if ls.read() == line_Sensor_forward:
+                print("Go Forward")
+            elif ls.read() == line_Sensor_left:
+                print("Turn Left")
+            elif ls.read() == line_Sensor_right:
+                print("Turn Right")
+            elif ls.read() == line_Sensor_slight_offtrack_left:
+                print("Offtrack left")
+            elif ls.read() == line_Sensor_slight_offtrack_right:
+                print("Offtrack right")
+            elif ls.read() == line_Sensor_offtrack:
+                print("Offtrack")
+            elif ls.read() == line_Sensor_left:
+                print("Turn Left")
+            elif ls.read() == line_Sensor_sharpleft:
+                print("Sharp Left")
+            elif ls.read() == line_Sensor_sharpright:
+                print("Sharp Right")
+            
+            
 
 if __name__ == "__main__":
     try:
