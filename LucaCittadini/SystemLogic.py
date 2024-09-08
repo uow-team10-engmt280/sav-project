906501699ec6c5283258f062713710c6af5c8c04
LucaCittadini <luca.cittadini13@gmail.com>
2024-09-08 21:07:22 +1200

    Update SystemLogic.py
    Date: 08 - 09 - 24
    Time: 2107

--- LucaCittadini/SystemLogic.py
+++ LucaCittadini/SystemLogic.py
@@ -1,3 +1,6 @@
+
+# TODO don't forget what classes are, they consist of attributes and methods, not just lines of code to be executed like "print()"
+
 import RPi.GPIO as GPIO # type: ignore 
 from time import time
 from typing import Protocol
@@ -20,7 +23,6 @@ class SAV:
         global forkFlag
         global pickDropFlag
         global mergeFlag
-        # global motorDriverMode
         global turnOne
         global turnTwo
         global pickUpSideOne
@@ -28,7 +30,7 @@ class SAV:
         global motorInstruc
         global pwmA
         global pwmB
-
+        global startTime    
 
         phaseA: int = 6
         enableA: int = 13
@@ -41,9 +43,9 @@ class SAV:
         forkFlag: bool = False
         pickDropFlag: bool = False
         mergeFlag: bool = False
-        # motorDriverMode: bool = False
 
         GPIO.setmode(GPIO.BCM)
+
         GPIO.setup(phaseA, GPIO.OUT)
         GPIO.setup(enableA, GPIO.OUT) 
         GPIO.setup(phaseB, GPIO.OUT)
@@ -62,44 +64,47 @@ class SAV:
         pwmA.start(0)
         pwmB.start(0)
 
-    def switch(self) -> None:
+    def switch(self, sav) -> None:
         self.state.switch(self)
-
+        
 class IDLE:
-    while(True):
-        match input('Type "Start" to begin program when you\'re ready: \n').lower():
-            case 's' | 'start':
-                break
-            case _:
-                print('Invalid, try again. \n')
+    def userWait() -> None:
+        while(True):
+            match input('Type "Start" to begin program when you\'re ready: \n').lower():
+                case 's' | 'start':
+                    break
+                case _:
+                    print('Invalid, try again. \n')
+    
     def switch(self, sav) -> None:
         sav.state = LISTENING()
-        print('Changing state')
+        print('Changing state to LISTENING')
 
 class LISTENING:
-    print('Entered LISTENING state successfully. \n')
+    def findPath() -> None:
+        TParray: list[bool] = MV() 
+        turnOne: bool = TParray(0)
+        pickUpSideOne: bool = TParray(1)
+        turnTwo: bool = TParray(2)
+        pickUpSideTwo: bool = TParray(3)
 
-    TParray: list[bool] = MV() 
-    turnOne: bool = TParray(0)
-    pickUpSideOne: bool = TParray(1)
-    turnTwo: bool = TParray(2)
-    pickUpSideTwo: bool = TParray(3)
-    while(True):
-        match input('Decisions received, type "Next" to start race: \n').lower():
-            case 'n' | 'next':
-                break
-            case _:
-                print('Invalid, try again. \n')
+    def userWait() -> None:
+        while(True):
+            match input('Decisions received, type "Next" to start race: \n').lower():
+                case 'n' | 'next':
+                    break
+                case _:
+                    print('Invalid, try again. \n')
 
-    global startTime
-    startTime: float = time()
+    def startStopWatch() -> None: # Move into userWait() method?
+        startTime: float = time()
+
     def switch(self, sav) -> None:
         sav.state = MOVING()
-        print('Changing state')
+        print('Changing state to MOVING')
 
 class MOVING:
-    print('Entered MOVING state successfully. \n')
-    def driving():
+    def driving() -> None:
         while(True): 
             rangeOut = bool(GPIO.input(rangeSense))
             if movPhase == 'phasePickDrop' | 'phasePark':
@@ -108,35 +113,34 @@ class MOVING:
                     pwmA.ChangeDutyCycle(0)
                     GPIO.output(phaseB, 0)
                     pwmB.ChangeDutyCycle(0)
+                    break
                 else:
                     motorInstruc = FindCase()
                     GPIO.output(phaseA, motorInstruc(0))
                     pwmA.ChangeDutyCycle(motorInstruc(1))
                     GPIO.output(phaseB, motorInstruc(2))
                     pwmB.ChangeDutyCycle(motorInstruc(3))
-                    
             else:
                 motorInstruc = FindCase()
                 GPIO.output(phaseA, motorInstruc(0))
                 pwmA.ChangeDutyCycle(motorInstruc(1))
                 GPIO.output(phaseB, motorInstruc(2))
                 pwmB.ChangeDutyCycle(motorInstruc(3))
-                
-
-
-    # Sends the decision to the motor driver to turn the sav accordingly
     
-    if pickDropFlag == False:
-        def switch(self, sav) -> None:
-            sav.state = PICKUP()
-            print('Changing to state PICKUP')
-    else:
-        def switch(self, sav) -> None:
-            sav.state = DROPOFF()
-            print('Changing to state DROPOFF')
+    def switch(self, sav) -> None:
+        match movPhase:
+            case 'phasePickDrop':
+                if pickDropFlag == False:
+                    sav.state = PICKUP()
+                    print('Changing state to PICKUP')
+                else:
+                    sav.state = DROPOFF()
+                    print('Changing state to DROPOFF')
+            case 'phasePark':
+                sav.state = PARKING()
+                print('Changing state to PARKING')
 
 class PICKUP:
-    print('Entered PICKUP state successfully. \n')
     # NOTE Should at some point indicate that this state has been jumped to so that sav moves to the next MOVING phase
     # Should stop the sav (send data to the motor driver)
     # Potentially change motor driver mode
@@ -145,33 +149,47 @@ class PICKUP:
     # Send data to small servo (some function)
     def switch(self, sav) -> None:
         sav.state = MOVING()
-        print('Changing state')
+        print('Changing state to MOVING')
 
 class DROPOFF:
-    print('Entered DROPOFF state successfully. \n')
     # Essentially it needs to complete the same task in PICKUP but maybe a different order or values
     def switch(self, sav) -> None:
         sav.state = MOVING()
-        print('Changing state')
+        print('Changing state to MOVING')
 
 class PARKING:
-    print('Entered PARKING state successfully. \n')
     # Should stop, re-adjust if needed 
     # NOTE not sure if any other tasks need to be complete during this state
     def switch(self, sav) -> None:
         sav.state = COMPLETE()
-        print('Changing state')
+        print('Changing state to COMPLETE')
 
 class COMPLETE:
-    print('Entered COMPLETE state successfully.')
-    endTime: float = time()
-    raceTime: float = endTime - startTime
-    minutes: int = raceTime//60
-    seconds: float = raceTime%60
-    print('You took %d minutes and %f seconds to complete the track. ' % (minutes, seconds))
+    def endStopWatch() -> None:
+        endTime: float = time()
+        raceTime: float = endTime - startTime
+        minutes: int = raceTime//60
+        seconds: float = raceTime%60
+        print('You took %d minutes and %f seconds to complete the track. ' % (minutes, seconds))
 
-    GPIO.cleanup()
+    def clean():
+        GPIO.cleanup()
+    
+    def switch(self, sav) -> None:
+        ...
+    
     # End programme nicely (stop processes and other programmes, have a nice complete message at the end)
 
 def main() -> None:
-    ...
+    sav = SAV()
+    sav.userWait()
+    sav.switch() # Move to LISTENING state
+
+    sav.findPath()
+    sav.userWait()
+    sav.startStopWatch()
+    sav.switch() # Move to MOVING state
+
+    sav.driving()
+    sav.switch() # Move to PARKING state
+    
