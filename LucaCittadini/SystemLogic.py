f7f80c7ec001ac968f32e14386814b9b72bbb69b
LucaCittadini <luca.cittadini13@gmail.com>
2024-09-08 19:44:43 +1200

    Update
    Date: 08 - 09 - 24
    Time: 1944
    Description:
    Added the logic for sending data to motor driver

--- LucaCittadini/SystemLogic.py
+++ LucaCittadini/SystemLogic.py
@@ -15,14 +15,33 @@ class SAV:
         global enableA
         global phaseB
         global enableB
+        global rangeSense
+        global movPhase
+        global forkFlag
+        global pickDropFlag
+        global mergeFlag
+        # global motorDriverMode
+        global turnOne
+        global turnTwo
+        global pickUpSideOne
+        global pickUpSideTwo
+        global motorInstruc
+        global pwmA
+        global pwmB
 
-        phaseA = 6
-        enableA = 13
-        phaseB = 19
-        enableB = 26
-        smallServoCtrl = 5
-        largeServoCtrl = 0
-        rangeSense = 2
+
+        phaseA: int = 6
+        enableA: int = 13
+        phaseB: int = 19
+        enableB: int = 26
+        smallServoCtrl: int = 5
+        largeServoCtrl: int = 0
+        rangeSense: int = 2
+        movPhase: str = 'phaseFork'
+        forkFlag: bool = False
+        pickDropFlag: bool = False
+        mergeFlag: bool = False
+        # motorDriverMode: bool = False
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(phaseA, GPIO.OUT)
@@ -38,22 +57,10 @@ class SAV:
         GPIO.output(phaseB, GPIO.LOW)
         GPIO.output(enableB, GPIO.LOW)
 
-        global movPhase
-        global forkFlag
-        global pickDropFlag
-        global mergeFlag
-        global motorDriverMode
-        global turnOne
-        global turnTwo
-        global pickUpSideOne
-        global pickUpSideTwo
-        global motorInstruc
-
-        movPhase: str = 'phaseFork'
-        forkFlag: bool = False
-        pickDropFlag: bool = False
-        mergeFlag: bool = False
-        motorDriverMode: bool = False
+        pwmA = GPIO.PWM(enableA, 1000)
+        pwmB = GPIO.PWM(enableB, 1000)
+        pwmA.start(0)
+        pwmB.start(0)
 
     def switch(self) -> None:
         self.state.switch(self)
@@ -70,7 +77,7 @@ class IDLE:
         print('Changing state')
 
 class LISTENING:
-    print('Entered new state successfully. \n')
+    print('Entered LISTENING state successfully. \n')
 
     TParray: list[bool] = MV() 
     turnOne: bool = TParray(0)
@@ -88,28 +95,33 @@ class LISTENING:
     startTime: float = time()
     def switch(self, sav) -> None:
         sav.state = MOVING()
-
+        print('Changing state')
 
 class MOVING:
-    print('Entered new state successfully. \n')
+    print('Entered MOVING state successfully. \n')
     def driving():
         while(True): 
+            rangeOut = bool(GPIO.input(rangeSense))
             if movPhase == 'phasePickDrop' | 'phasePark':
-                if rangeSense == 1:
-                    ... # Stop driving/moving
+                if rangeOut == True:
+                    GPIO.output(phaseA, 0)
+                    pwmA.ChangeDutyCycle(0)
+                    GPIO.output(phaseB, 0)
+                    pwmB.ChangeDutyCycle(0)
                 else:
                     motorInstruc = FindCase()
-                    # phaseA = motorInstruc(0)
-                    # enableA = motorInstruc(1)
-                    # phaseB = motorInstruc(2)
-                    # enableB = motorInstruc(3)
-
+                    GPIO.output(phaseA, motorInstruc(0))
+                    pwmA.ChangeDutyCycle(motorInstruc(1))
+                    GPIO.output(phaseB, motorInstruc(2))
+                    pwmB.ChangeDutyCycle(motorInstruc(3))
+                    
             else:
                 motorInstruc = FindCase()
-                # phaseA = motorInstruc(0)
-                # enableA = motorInstruc(1)
-                # phaseB = motorInstruc(2)
-                # enableB = motorInstruc(3)
+                GPIO.output(phaseA, motorInstruc(0))
+                pwmA.ChangeDutyCycle(motorInstruc(1))
+                GPIO.output(phaseB, motorInstruc(2))
+                pwmB.ChangeDutyCycle(motorInstruc(3))
+                
 
 
     # Sends the decision to the motor driver to turn the sav accordingly
@@ -117,12 +129,14 @@ class MOVING:
     if pickDropFlag == False:
         def switch(self, sav) -> None:
             sav.state = PICKUP()
+            print('Changing to state PICKUP')
     else:
         def switch(self, sav) -> None:
             sav.state = DROPOFF()
+            print('Changing to state DROPOFF')
 
 class PICKUP:
-    print('Entered new state successfully. \n')
+    print('Entered PICKUP state successfully. \n')
     # NOTE Should at some point indicate that this state has been jumped to so that sav moves to the next MOVING phase
     # Should stop the sav (send data to the motor driver)
     # Potentially change motor driver mode
@@ -131,28 +145,32 @@ class PICKUP:
     # Send data to small servo (some function)
     def switch(self, sav) -> None:
         sav.state = MOVING()
+        print('Changing state')
 
 class DROPOFF:
-    print('Entered new state successfully. \n')
+    print('Entered DROPOFF state successfully. \n')
     # Essentially it needs to complete the same task in PICKUP but maybe a different order or values
     def switch(self, sav) -> None:
         sav.state = MOVING()
+        print('Changing state')
 
 class PARKING:
-    print('Entered new state successfully. \n')
+    print('Entered PARKING state successfully. \n')
     # Should stop, re-adjust if needed 
     # NOTE not sure if any other tasks need to be complete during this state
     def switch(self, sav) -> None:
         sav.state = COMPLETE()
+        print('Changing state')
 
 class COMPLETE:
-    print('Entered new state successfully.')
+    print('Entered COMPLETE state successfully.')
     endTime: float = time()
     raceTime: float = endTime - startTime
     minutes: int = raceTime//60
     seconds: float = raceTime%60
     print('You took %d minutes and %f seconds to complete the track. ' % (minutes, seconds))
 
+    GPIO.cleanup()
     # End programme nicely (stop processes and other programmes, have a nice complete message at the end)
 
 def main() -> None:
