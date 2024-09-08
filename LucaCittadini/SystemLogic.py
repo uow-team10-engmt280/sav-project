99ef51bf1f02bc57f737beaec96592d8947830c8
LucaCittadini <luca.cittadini13@gmail.com>
2024-09-08 18:22:59 +1200

    Update - Added files, sorted folders
    Date: 08 - 09 - 24
    Time: 1821
    Description:
    Added another text document for information and reminders of what needs doing within the code

--- LucaCittadini/SystemLogic.py
+++ LucaCittadini/SystemLogic.py
@@ -1,39 +1,3 @@
-''' # TODO should put this into a seperate README file or something along those lines, useful for report. 
-This programme is the center of the operation and ties the other programmes together.
-
-It is built off of a Finite State Machine framework, using classes as the states. 
-
-It will first import the Machine Vision programme's main function in to determine both 
-the turns the SAV needs to take, as well as the side of pickup and dropoff. 
-
-In the MOVING state we will be importing a list of booleans from the reflectance 
-sensor programme that represent the part of the track we are currently driving on, from 
-this list we then determine the corresponding/required motor speeds to keep the SAV on 
-the track. 
-
-This programme also implements the logic required to pickup and dropoff the 
-lego man as well as park, it will be accessing data from the range sensor.
-
-NOTE Could put the range sensor logic into a seperate programme, may need to see how 
-much this affects perform, but the code would be pretty simple (maybe motor driver too?)
-'''
-
-# TODO should organise each file that needs running so that the entire file is a function that we can import 
-
-# IDLE --> LISTENING
-# LISTENING --> MOVING
-# MOVING --> PICKUP
-# PICKUP --> MOVING
-# MOVING --> DROPOFF
-# DROPOFF --> MOVING
-# MOVING --> PARKING
-# PARKING --> COMPLETED
-
-'''
-NOTE 
-- May want to create some functions that make the servos do specific tasks (will be reused in PICKUP and DROPOFF)
-
-'''
 import RPi.GPIO as GPIO # type: ignore 
 from time import time
 from typing import Protocol
@@ -60,22 +24,19 @@ class SAV:
         largeServoCtrl = 0
         rangeSense = 2
 
-
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(phaseA, GPIO.OUT)
-        GPIO.setup(enableA, GPIO.OUT) # THIS PIN SUPPORTS PWM
+        GPIO.setup(enableA, GPIO.OUT) 
         GPIO.setup(phaseB, GPIO.OUT)
-        GPIO.setup(enableB, GPIO.OUT) # THIS PIN DOES NOT SUPPORT PWM, GPIO12 DOES SUPPORT PWM
-        GPIO.setup(largeServoCtrl, GPIO.OUT) # SMALL SERVO
-        GPIO.setup(smallServoCtrl, GPIO.OUT) # LARGE SERVO
-        GPIO.setup(rangeSense, GPIO.IN) # RANGE SENSOR
+        GPIO.setup(enableB, GPIO.OUT) 
+        GPIO.setup(largeServoCtrl, GPIO.OUT) 
+        GPIO.setup(smallServoCtrl, GPIO.OUT) 
+        GPIO.setup(rangeSense, GPIO.IN) 
         
         GPIO.output(phaseA, GPIO.LOW)
         GPIO.output(enableA, GPIO.LOW)
         GPIO.output(phaseB, GPIO.LOW)
         GPIO.output(enableB, GPIO.LOW)
-
-
 
         global movPhase
         global forkFlag
@@ -94,8 +55,6 @@ class SAV:
         mergeFlag: bool = False
         motorDriverMode: bool = False
 
-
-
     def switch(self) -> None:
         self.state.switch(self)
 
@@ -112,17 +71,12 @@ class IDLE:
 
 class LISTENING:
     print('Entered new state successfully. \n')
-    
-    # May want to wait until the Machine Vision programme is fully done
 
     TParray: list[bool] = MV() 
     turnOne: bool = TParray(0)
     pickUpSideOne: bool = TParray(1)
     turnTwo: bool = TParray(2)
     pickUpSideTwo: bool = TParray(3)
-    
-
-    # Start reflectance sensor programme
     while(True):
         match input('Decisions received, type "Next" to start race: \n').lower():
             case 'n' | 'next':
@@ -175,14 +129,12 @@ class PICKUP:
     # Potentially fix position (to ensure it picks up in right place)
     # Send data to large servo (some function, will need to go up and down)
     # Send data to small servo (some function)
-    # Once PICKUP is complete, should change state like done here:
     def switch(self, sav) -> None:
         sav.state = MOVING()
 
 class DROPOFF:
     print('Entered new state successfully. \n')
     # Essentially it needs to complete the same task in PICKUP but maybe a different order or values
-    # Once DROPOFF is complete, move to next state like done here:
     def switch(self, sav) -> None:
         sav.state = MOVING()
 
@@ -202,8 +154,6 @@ class COMPLETE:
     print('You took %d minutes and %f seconds to complete the track. ' % (minutes, seconds))
 
     # End programme nicely (stop processes and other programmes, have a nice complete message at the end)
-
-
 
 def main() -> None:
     ...
