52ccfa563963d3ef171fe7e0eeb3633767498feb
taroBense <140390456+taroBense@users.noreply.github.com>
2024-09-26 08:28:15 +1200

    update

--- interfacing/linefollower.py
+++ interfacing/linefollower.py
@@ -3,3 +3,29 @@ import RPi.GPIO as GPIO
 from linesensor import LineSensor
 
 
+def calc_error(LineSensor):
+    weights = [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+    error_total = 0
+    total_weighted = 0
+
+    for channel, value in enumerate(LineSensor.sensor_values):
+        if value == 1:
+            error_total += weights[channel]
+            total_weighted += 1
+
+    error = error_total / total_weighted
+
+    if error > 0:
+        return error
+    else:
+        return None
+    
+# def pid_controller(error, dt):
+#     global integral, previous_error
+
+#     proportional = Kp * error
+#     integral += error * dt
+#     derivative = (error - previous_error) / dt
+#     previous_error = error
+
+#     return proportional + (Ki * integral) + (Kd * derivative)
